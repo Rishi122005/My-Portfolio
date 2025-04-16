59f917f93864abc3eaebdaeb1640f35469c91259
Rishi Raj <rr1158@srmist.edu.in>
2025-04-16 20:40:22 +0530

    Initial portfolio files

---
+++ components/sub/hero-content.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/lib/motion";
+
+export const HeroContent = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+      <a href="/resume.pdf" download>
+  <motion.div
+    variants={slideInFromTop}
+    className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9] cursor-pointer"
+  >
+    <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+    <h1 className="Welcome-text text-[13px]">
+      My Resume
+    </h1>
+  </motion.div>
+</a>
+
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            HI{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              I'm
+            </span>{" "}
+            Rishi
+          </span>
+        </motion.div>
+
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          I&apos;m a Full Stack Software Engineer with experience in Website,
+          Mobile, and Software development. Check out my projects and skills.
+        </motion.p>
+        
+
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Education
+          
+        </motion.a>
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+         1. B.Tech in Computer Science Engineering
+            SRM Institute of Science and Technology, Kattankulathur
+
+          Pursuing since 2023
+
+          Current GPA: 8.3
+
+          Actively gaining technical and soft skills through coursework, projects, and hands-on experience
+
+          Focused on continuous improvement and aiming to increase overall GPA
+          </motion.p>
+
+          <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+        2. Senior Secondary (Class 12)
+Himalayan Public School, Patna
+
+Passed in 2023 with an aggregate of 73.4%
+
+Developed strong analytical and logical reasoning skills
+
+Gained interest in STEM subjects, particularly in engineering and technology
+          </motion.p>
+
+          <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+         3. Secondary (Class 10)
+Delhi Public School, Patna
+
+Passed in 2021 with 91.4%
+
+Built a solid academic foundation with consistent performance
+
+Participated in school-level competitions and extracurricular activities
+
+
+          </motion.p>
+         
+      </div>
+
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/hero-bg.svg"
+          alt="work icons"
+          height={650}
+          width={650}
+          draggable={false}
+          className="select-none"
+        />
+      </motion.div>
+    </motion.div>
+  );
+};
---
+++ constants/index.ts
@@ -0,0 +1,343 @@
+import {FaFacebook } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const SKILL_DATA = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    image: "framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Stripe",
+    image: "stripe.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+] as const;
+
+export const SOCIALS = [
+  {
+    name: "Instagram",
+    icon: RxInstagramLogo,
+    link: "https://www.instagram.com/rishi_raj_077/",
+  },
+] as const;
+
+export const FRONTEND_SKILL = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Material UI",
+    image: "mui.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const BACKEND_SKILL = [
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express.js",
+    image: "express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: "Firebase",
+    image: "firebase.png",
+    width: 55,
+    height: 55,
+  },
+  {
+    skill_name: "PostgreSQL",
+    image: "postgresql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "MySQL",
+    image: "mysql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Prisma",
+    image: "prisma.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Graphql",
+    image: "graphql.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const FULLSTACK_SKILL = [
+  {
+    skill_name: "React Native",
+    image: "reactnative.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Tauri",
+    image: "tauri.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Docker",
+    image: "docker.png",
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: "Figma",
+    image: "figma.png",
+    width: 50,
+    height: 50,
+  },
+] as const;
+
+export const OTHER_SKILL = [
+  {
+    skill_name: "Go",
+    image: "go.png",
+    width: 60,
+    height: 60,
+  },
+] as const;
+
+export const PROJECTS = [
+  {
+    title: "Trading Portfolio Website",
+    description:
+      'DappRush is a modern, visually dynamic trading website designed to simplify and elevate the user experience in the world of decentralized finance (DeFi). Built with a sleek black, gold, and brown theme, DappRush offers a futuristic and immersive interface featuring glowing cryptocurrency icons and interactive 3D models of popular coins like Bitcoin and Ethereum. These elements not only enhance the visual appeal but also keep users engaged while navigating the platform. With smooth animations and a responsive layout, DappRush caters to both novice and experienced traders, providing real-time market data, intuitive trading tools, and secure blockchain integration. Whether you are tracking market trends or managing your crypto portfolio, DappRush is built to deliver a seamless, tech-forward experience that stands out in the crypto space',
+    image: "/projects/project-1.png",
+    link: "https://example.com",
+  },
+  {
+    title: "Weather Forecasting ",
+    description:
+      'I created a weather forecasting app that provides real time weather updates for any location around the world. The app is designed with a user friendly interface and displays key information such as temperature, humidity, wind speed, and weather conditions in a simple and organized manner. It uses reliable weather API data to ensure accurate and timely forecasts. The app is responsive across different devices and offers a smooth experience whether used on desktop or mobile. It is a useful tool for anyone who wants to stay informed about weather changes quickly and efficiently.',
+    image: "/projects/project-2.png",
+    link: "https://weather-dashboard-master-kigt1o831-rishi122005s-projects.vercel.app/",
+  },
+  {
+    title: "Space Themed Website",
+    description:
+      'My portfolio website is a reflection of my personal and professional journey, showcasing my skills, creativity, and dedication. It includes a detailed About Me section where visitors can learn more about my background, interests, and career goals. The Projects section highlights some of the key work I have done, featuring a variety of practical and creative projects that demonstrate my growth and capabilities. Each project is displayed with relevant details to provide context and insight into the process. The site also includes a Contact Me section, making it easy for anyone to get in touch with me for collaboration, opportunities, or feedback. The overall design is clean, responsive, and easy to navigate, offering a seamless user experience.',
+    image: "/projects/project-3.png",
+    link: "https://example.com",
+  },
+] as const;
+
+export const FOOTER_DATA = [
+  {
+    title: "Community",
+    data: [
+      {
+        name: "GitHub",
+        icon: RxGithubLogo,
+        link: "https://github.com/Rishi122005",
+      },
+      {
+        name: "Discord",
+        icon: RxDiscordLogo,
+        link: "https://discord.com",
+      },
+    ],
+  },
+  {
+    title: "Social Media",
+    data: [
+      {
+        name: "Instagram",
+        icon: RxInstagramLogo,
+        link: "https://www.instagram.com/rishi_raj_077/",
+      },
+      {
+        name: "Linkedin",
+        icon: RxLinkedinLogo,
+        link: "https://www.linkedin.com/in/rishi122005/",
+      },
+    ],
+  },
+  {
+    title: "About",
+    data: [
+      {
+        name: "Become my Client",
+        icon: null,
+        link: "https://www.upwork.com/freelancers/~019c99dab419a7bbde",
+      },
+      {
+        name: "Know About me on Upwork",
+        icon: null,
+        link: "https://www.upwork.com/freelancers/~019c99dab419a7bbde",
+      },
+      {
+        name: "Contact Me",
+        icon: null,
+        link: "mailto:codrishiriengs@gmail.com",
+      },
+    ],
+  },
+] as const;
+
+export const NAV_LINKS = [
+  {
+    title: "About me",
+    link: "#about-me",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+] as const;
+
+export const LINKS = {
+  sourceCode: "https://github.com/sanidhyy/space-portfolio",
+};
